3b1de04b03f1b66c01f259065201a4bd3afca973
Anisur Rahman Nayan <arnayan07sept.com>
2021-11-25 22:22:59 +0600

    updated

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.use(bodyParser.urlencoded({extended:true}))
 app.set('view engine', 'ejs');
 app.use(express.static("Public"))
 
-const port = 3000
+const port = process.env.PORT || 3000;
 
 var items = [];
 
